f7d3f4e62937252c11d23cb3fcb70c518be8f56e
jashlukhi <102151399+jashlukhi@users.noreply.github.com>
2024-02-23 16:22:47 +0530

    F UP

---
+++ src/content_option.js
@@ -0,0 +1,185 @@
+import Main from "./assets/Main.webp";
+import Portfolio_1 from "./assets/Portfolio 1.webp";
+import Portfolio_2 from "./assets/Portfolio 2.webp";
+import Portfolio_3 from "./assets/Portfolio 3.webp";
+import Portfolio_4 from "./assets/Portfolio 4.webp";
+import Portfolio_5 from "./assets/Portfolio 5.webp";
+import Portfolio_6 from "./assets/Portfolio 6.png";
+
+
+const logotext = "JASH LUKHI";
+
+const meta = {
+    title: "Jash Lukhi",
+    description: "I’m Jash Lukhi data scientist _ Full stack devloper,currently working in India",
+};
+
+const introdata = {
+    title: "I’m Jash Lukhi",
+    animated: {
+        first: "I'm Full Stack Developer",
+        second: "I'm Websites Designer",
+        third: "I'm Develop Web Apps",
+    },
+    description: "I possess a deep understanding of both front-end and back-end development, allowing me to create robust and user-friendly web applications.",
+    your_img_url: Main,
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "I’m Jash, currently I live in Surat working as a senior Full Stack Developer at Keepitech. My skill set includes HTML, CSS, JavaScript, jQuery, React, Express.js, Node.js, php, and proficiency in database management systems like MySQL and MongoDb. I constantly stay updated with new technologies and best practices to ensure that I can provide clients and users with the best digital solutions.",
+};
+
+const worktimeline = [{
+    jobtitle: "MERN Stack Developer",
+    where: "KeepiTech",
+    date: "2024",
+},
+{
+    jobtitle: "Team Leade",
+    where: "Snepitech",
+    date: "2023",
+},
+{
+    jobtitle: "Full Stack Developer",
+    where: "Snepitech",
+    date: "2022",
+},
+];
+
+const skills = [{
+    name: "HTML",
+    value: 95,
+},
+{
+    name: "CSS | CSS3",
+    value: 90,
+},
+{
+    name: "SASS",
+    value: 90,
+},
+{
+    name: "Javascript",
+    value: 95,
+},
+{
+    name: "Jquery",
+    value: 85,
+},
+{
+    name: "React",
+    value: 95,
+},
+{
+    name: "Next.JS",
+    value: 85,
+},
+{
+    name: "Mongo DB",
+    value: 90,
+},
+{
+    name: "Express JS",
+    value: 90,
+},
+{
+    name: "Node JS",
+    value: 90,
+},
+{
+    name: "Socket.IO",
+    value: 80,
+},
+{
+    name: "WordPress",
+    value: 90,
+},
+{
+    name: "Shopify",
+    value: 90,
+},
+];
+
+const services = [{
+    title: "Full Stack Developer",
+    description: "Versatile Full Stack Developer | Crafting Seamless Digital Experiences with Expertise in Frontend and Backend Technologies.",
+},
+{
+    title: "Web Design",
+    description: "Crafting digital excellence: Elevate your online presence with a freelance web designer passionate about creating stunning and user-centric websites.",
+},
+{
+    title: "Wordpress Design",
+    description: "Crafting captivating WordPress designs that seamlessly blend creativity and functionality, I bring digital visions to life for clients seeking a dynamic online presence.",
+},
+{
+    title: "Shopify Design",
+    description: "Crafting captivating Shopify experiences that blend creativity with functionality, elevating online storefronts to new heights.",
+},
+];
+
+const dataportfolio = [{
+    img: Portfolio_1,
+    description: "Keepitech | Provide Best It Services For Your Business",
+    link: "https://keepitech.com/",
+},
+{
+    img: Portfolio_2,
+    description: "Depth Analysis For Crypto Coin",
+    link: "",
+},
+{
+    img: Portfolio_5,
+    description: "JAY AMAR GEMS | All Jewellery Specialist",
+    link: "https://www.jayamargems.com/",
+},
+{
+    img: Portfolio_4,
+    description: "AJ DiamondIndia | All Fancy Shape Diamond There Available",
+    link: "https://ajdiamondindia.com/",
+},
+{
+    img: Portfolio_6,
+    description: "yumycafe | FRESH & TASTY FOODS",
+    link: "https://jashlukhi.github.io/yumycafe/",
+},
+{
+    img: Portfolio_3,
+    description: "James Smith | Personal Portfolio",
+    link: "",
+},
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "lukhijash@gmail.com",
+    YOUR_FONE: "63534 49734",
+    description: "Jash is a professional web developer. Feel free to get in touch with me.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_rv2d50z",
+    YOUR_TEMPLATE_ID: "template_vhwgxhr",
+    YOUR_USER_ID: "s_RBMVu1LCuz6DvT4",
+};
+
+const socialprofils = {
+    github: "https://github.com/jashlukhi",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/jash-lukhi-60786a243",
+    twitter: "https://twitter.com",
+    instagram: "https://www.instagram.com/jash___patel",
+};
+
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
